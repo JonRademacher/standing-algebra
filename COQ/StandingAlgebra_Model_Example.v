a88fb4cdaf5d441601f86a21e47feb2bbd238522
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-04-10 11:31:55 -0500

    Refactor apply identity hypotheses and proofs

--- COQ/StandingAlgebra_Model_Example.v
+++ COQ/StandingAlgebra_Model_Example.v
@@ -24,8 +24,11 @@ Variable F_id : Operation.
 Hypothesis sigma_const :
   forall a : Agent, σ a = 1.
 
-Hypothesis apply_identity :
-  forall (a : Agent), apply F_id a = a.
+Hypothesis apply_identity_agent :
+  forall (a : Agent), ApplyAgent F_id a = a.
+
+Hypothesis apply_identity_state :
+  forall (s : State), ApplyState F_id s = s.
 
 Hypothesis dompressure_zero :
   DomPressure S0 = 0.
@@ -45,33 +48,34 @@ Hypothesis F_id_not_repair :
 
 Lemma model_standing_preserved :
   forall a : Agent,
-    σ (apply F_id a) >= σ a.
+    σ (ApplyAgent F_id a) >= σ a.
 Proof.
   intro a.
-  rewrite apply_identity.
+  rewrite apply_identity_agent.
+  rewrite sigma_const.
   lia.
 Qed.
 
 Lemma model_idempotent :
   forall a : Agent,
-    apply F_id (apply F_id a) = apply F_id a.
+    ApplyAgent F_id (ApplyAgent F_id a) = ApplyAgent F_id a.
 Proof.
   intro a.
-  repeat rewrite apply_identity.
+  repeat rewrite apply_identity_agent.
   reflexivity.
 Qed.
 
 Lemma model_dompressure_monotone :
-  DomPressure (apply F_id S0) <= DomPressure S0.
+  DomPressure (ApplyState F_id S0) <= DomPressure S0.
 Proof.
-  rewrite apply_identity.
+  rewrite apply_identity_state.
   rewrite dompressure_zero.
   lia.
 Qed.
 
 Lemma model_legitimate_safe :
   forall s : State,
-    DomPressure (apply F_id s) <= DomPressure s.
+    DomPressure (ApplyState F_id s) <= DomPressure s.
 Proof.
   intros s.
   apply DomPressure_monotonic.
@@ -79,4 +83,3 @@ Proof.
 Qed.
 
 End SigmaR_Model_Example.
-``
