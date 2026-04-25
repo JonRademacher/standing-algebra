d7cf3aa1451799eab76ce358955643f71113b998
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-04-25 08:52:10 -0500

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -1,16 +1,11 @@
 import Lake
 open Lake DSL
 
-/--
-Standing Algebra (Σᴿ)
-
-Package configuration only.
-No theory code belongs in this file.
--/
 package standing_algebra where
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
 
 @[default_target]
-lean_lib SigmaR
+lean_lib SigmaR where
+  srcDir := "SigmaR"
