ccf4e0da4d24196f9ec381557c1795ee54389b7f
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-04-28 09:44:10 -0500

    Create ExitCollapseNotStanding.lean

---
+++ SigmaR/Valuation/Measures/Independence/ExitCollapseNotStanding.lean
@@ -0,0 +1,27 @@
+import SigmaR.StandingAlgebra_FormalCore
+import SigmaR.Valuation.Measures.ExitCollapse_Definition
+import SigmaR.Valuation.Measures.StandingMeasure_Definition
+
+/-!
+###############################################################################
+# Independence: Exit Collapse ≠ Standing
+###############################################################################
+
+Exit collapse does not entail loss of standing.
+
+Recognition or admissible participation may persist
+despite the absence of viable exit.
+###############################################################################
+-/
+
+namespace SigmaR
+
+/--
+There is no general implication from exit collapse
+to loss of standing.
+-/
+axiom exit_collapse_not_standing :
+  ¬ (∀ (a : Agent) (s : State),
+       ExitCollapse a s → ¬ StandingMeasure a s)
+
+end SigmaR
