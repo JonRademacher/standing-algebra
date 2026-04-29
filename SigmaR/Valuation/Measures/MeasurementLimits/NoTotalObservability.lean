5e449e1ebdb5dabfd4ebe65ae3df092753320e56
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-04-29 10:54:51 -0500

    Update NoTotalObservability.lean

--- SigmaR/Valuation/Measures/MeasurementLimits/NoTotalObservability.lean
+++ SigmaR/Valuation/Measures/MeasurementLimits/NoTotalObservability.lean
@@ -7,17 +7,21 @@ import SigmaR.StandingAlgebra_FormalCore
 
 There is no agent for whom the entire system state
 is fully observable.
+
+No observer has complete access to all states.
 ###############################################################################
 -/
 
 namespace SigmaR
 
 /--
-No agent has total observability of all states.
+No agent is assumed to observe all states.
 -/
 axiom no_total_observability :
-  ¬ (∃ (a : Agent),
-        ∀ (s : State),
-          True)
+  ¬ (
+    ∃ (a : Agent),
+      ∀ (s : State),
+        Observable a s
+  )
 
 end SigmaR
