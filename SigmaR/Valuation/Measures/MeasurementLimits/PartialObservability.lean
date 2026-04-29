1fce217e942519926a447b6a66f9cd1adf760fa4
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-04-29 10:56:14 -0500

    Update PartialObservability.lean

--- SigmaR/Valuation/Measures/MeasurementLimits/PartialObservability.lean
+++ SigmaR/Valuation/Measures/MeasurementLimits/PartialObservability.lean
@@ -7,16 +7,20 @@ import SigmaR.StandingAlgebra_FormalCore
 
 Not all aspects of the system are observable
 by any agent at any time.
+
+Each agent has blind spots.
 ###############################################################################
 -/
 
 namespace SigmaR
 
 /--
-Observability is partial.
+Observability is partial: for every agent, there exist
+states that are not observable to that agent.
 -/
 axiom partial_observability :
-  ∃ (a : Agent) (s : State),
-    True
+  ∀ (a : Agent),
+    ∃ (s : State),
+      ¬ Observable a s
 
 end SigmaR
