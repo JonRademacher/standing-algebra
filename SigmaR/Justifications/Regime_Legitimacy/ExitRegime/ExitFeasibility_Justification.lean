d4eccfbd0a0edd32fe2bfc9c3cae9d74c104dbba
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-04-30 16:10:39 -0500

    Update ExitFeasibility_Justification.lean

--- SigmaR/Justifications/Regime_Legitimacy/ExitRegime/ExitFeasibility_Justification.lean
+++ SigmaR/Justifications/Regime_Legitimacy/ExitRegime/ExitFeasibility_Justification.lean
@@ -1,3 +1,5 @@
+module SigmaR.Justifications.Regime_Legitimacy.ExitRegime.ExitFeasibility_Justification
+
 import SigmaR.Justifications.Core_Legitimacy.CIA_Justification
 import SigmaR.Justifications.Regimes.ExitRegime.ExitAvailability_Justification
 
