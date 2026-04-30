15fde14f6cd67a75ed8549726cfcb9e79eeee464
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-04-30 14:52:06 -0500

    Create Response.lean

---
+++ SigmaR/Valuation/ResponseSemantics/Response.lean
@@ -0,0 +1,30 @@
+import SigmaR.StandingAlgebra_FormalCore
+
+/-!
+###############################################################################
+# Response
+###############################################################################
+
+This file introduces the abstract notion of a response.
+
+A response represents a possible structural transformation
+between states, without implying:
+• desirability
+• legitimacy
+• authority
+• obligation
+• intervention
+
+It is purely descriptive.
+###############################################################################
+-/
+
+namespace SigmaR
+
+/--
+An abstract response representing a possible transformation
+of system state.
+-/
+constant Response : Type
+
+end SigmaR
