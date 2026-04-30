84a2befdeac4998746501f7d26ac19defabe8f79
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-04-30 16:10:56 -0500

    Update ExitNonPunitive_Justification.lean

--- SigmaR/Justifications/Regime_Legitimacy/ExitRegime/ExitNonPunitive_Justification.lean
+++ SigmaR/Justifications/Regime_Legitimacy/ExitRegime/ExitNonPunitive_Justification.lean
@@ -1,3 +1,5 @@
+module SigmaR.Justifications.Regime_Legitimacy.ExitRegime.ExitNonPunitive_Justification
+
 import SigmaR.Justifications.Core_Legitimacy.CIA_Justification
 import SigmaR.Justifications.Regimes.ExitRegime.ExitRegime_Justification
 
