c42bd05fcb3a24e709fa2b6dfbb2c8ba50f66204
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-04-30 16:12:22 -0500

    Update RiskConsentNonTransitivity_Justification.lean

--- SigmaR/Justifications/Regime_Legitimacy/RiskRegime/RiskConsentNonTransitivity_Justification.lean
+++ SigmaR/Justifications/Regime_Legitimacy/RiskRegime/RiskConsentNonTransitivity_Justification.lean
@@ -1,3 +1,5 @@
+module SigmaR.Justifications.Regime_Legitimacy.RiskRegime.RiskConsentNonTransivity_Justification
+
 import SigmaR.Justifications.Core_Legitimacy.CIA_Justification
 import SigmaR.Justifications.Core_Legitimacy.NRPP_Justification
 import SigmaR.Justifications.Regimes.RiskRegime.RiskRegime_Justification
