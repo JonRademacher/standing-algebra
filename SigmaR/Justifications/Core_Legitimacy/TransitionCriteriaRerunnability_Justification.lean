c72c3eaa49d2026d62a654fd3dc647c802a42bbc
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-04-30 15:43:43 -0500

    Update TransitionCriteriaRerunnability_Justification.lean

--- SigmaR/Justifications/Core_Legitimacy/TransitionCriteriaRerunnability_Justification.lean
+++ SigmaR/Justifications/Core_Legitimacy/TransitionCriteriaRerunnability_Justification.lean
@@ -1,3 +1,5 @@
+module SigmaR.Justifications.Core_Legitimacy.TransitionCriteriaRerunnability_Justification
+
 import SigmaR.Justifications.Core_Legitimacy.PFA_Justification
 import SigmaR.Justifications.Core_Legitimacy.ContestationCondition_Justification
 import SigmaR.Justifications.Core_Legitimacy.ScopeScaleRelativity_Justification
