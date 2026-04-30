3a9aceb96a3c94b11bda63540792665f6b6ee2e7
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-04-30 14:21:53 -0500

    Create AdmissibilityDoesNotImplyValidity.lean

---
+++ SigmaR/Valuation/Admissibility/AdmissibilityDoesNotImplyValidity.lean
@@ -0,0 +1,22 @@
+import SigmaR.StandingAlgebra_FormalCore
+
+/-!
+###############################################################################
+# Admissibility Does Not Imply Validity
+###############################################################################
+
+Admissibility does not imply logical validity or soundness
+of any inference.
+
+This file introduces no axioms.
+###############################################################################
+-/
+
+namespace SigmaR
+
+/--
+Admissibility of reasoning does not imply logical validity.
+-/
+constant AdmissibilityDoesNotImplyValidity : Prop
+
+end SigmaR
