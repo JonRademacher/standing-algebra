77e64d49e479e82414642bfa8557fd5da71cfba6
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-04-30 16:20:53 -0500

    Update RemedyRegime_Justification.lean

--- SigmaR/Justifications/Regime_Legitimacy/RemedyRegime/RemedyRegime_Justification.lean
+++ SigmaR/Justifications/Regime_Legitimacy/RemedyRegime/RemedyRegime_Justification.lean
@@ -1,3 +1,5 @@
+module SigmaR.Justifications.Regime_Legitimacy.RemedyRegime.RemedyRegime_Justification
+
 import SigmaR.Justifications.Core_Legitimacy.StructuralDebt_Justification
 import SigmaR.Justifications.Core_Legitimacy.DirectedRepair_Justification
 import SigmaR.Justifications.Core_Legitimacy.LegitimacyEnvelope_Justification
