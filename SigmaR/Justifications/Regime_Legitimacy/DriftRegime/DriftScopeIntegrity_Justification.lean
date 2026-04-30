5a5c0fd0d215b88751afbf7207611adc93cef7eb
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-04-30 16:05:40 -0500

    Update DriftScopeIntegrity_Justification.lean

--- SigmaR/Justifications/Regime_Legitimacy/DriftRegime/DriftScopeIntegrity_Justification.lean
+++ SigmaR/Justifications/Regime_Legitimacy/DriftRegime/DriftScopeIntegrity_Justification.lean
@@ -1,3 +1,5 @@
+module SigmaR.Justifications.Regime_Legitimacy.DriftRegime.DriftScopeIntegrity_Justification
+
 import SigmaR.Justifications.Regimes.AdapterRegime.AdapterScopeIntegrity_Justification
 import SigmaR.Justifications.Regimes.DriftRegime.DriftRegime_Justification
 
