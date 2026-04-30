41e075cb939c522d1b82a8c06b0951394a37eec1
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-04-30 15:21:21 -0500

    Update CorePreconditions.lean

--- SigmaR/Justifications/Core_Legitimacy/CorePreconditions.lean
+++ SigmaR/Justifications/Core_Legitimacy/CorePreconditions.lean
@@ -1,3 +1,5 @@
+module SigmaR.Justifications.Core_Legitimacy.CorePreconditions
+
 import SigmaR.Justifications.Core_Legitimacy.PFA_Justification
 import SigmaR.Justifications.Core_Legitimacy.ContestationCondition_Justification
 import SigmaR.Justifications.Core_Legitimacy.PublicFrameResolution_Justification
