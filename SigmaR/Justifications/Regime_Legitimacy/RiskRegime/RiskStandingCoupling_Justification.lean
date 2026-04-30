296593f9e03a065c0b04961fb7d226acba685b3c
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-04-30 16:14:09 -0500

    Update RiskStandingCoupling_Justification.lean

--- SigmaR/Justifications/Regime_Legitimacy/RiskRegime/RiskStandingCoupling_Justification.lean
+++ SigmaR/Justifications/Regime_Legitimacy/RiskRegime/RiskStandingCoupling_Justification.lean
@@ -1,3 +1,5 @@
+module SigmaR.Justifications.Regime_Legitimacy.RiskRegime.RiskStandingCoupling_Justification
+
 import SigmaR.Justifications.Core_Legitimacy.CIA_Justification
 import SigmaR.Justifications.Core_Legitimacy.StructuralDebt_Justification
 import SigmaR.Justifications.Core_Legitimacy.DirectedRepair_Justification
