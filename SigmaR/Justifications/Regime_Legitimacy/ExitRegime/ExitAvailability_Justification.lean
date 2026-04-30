c14b3bfbedd970d21708368c696269ed824ed7a2
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-04-30 16:10:21 -0500

    Update ExitAvailability_Justification.lean

--- SigmaR/Justifications/Regime_Legitimacy/ExitRegime/ExitAvailability_Justification.lean
+++ SigmaR/Justifications/Regime_Legitimacy/ExitRegime/ExitAvailability_Justification.lean
@@ -1,3 +1,5 @@
+module SigmaR.Justifications.Regime_Legitimacy.ExitRegime.ExitAvailability_Justification
+
 import SigmaR.Justifications.Core_Legitimacy.CIA_Justification
 import SigmaR.Justifications.Core_Legitimacy.StructuralDebt_Justification
 
