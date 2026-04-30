b49be86c3795c4f08d9e2e450214805a7d2d3598
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-04-30 14:52:32 -0500

    Create ResponseSet.lean

---
+++ SigmaR/Valuation/ResponseSemantics/ResponseSet.lean
@@ -0,0 +1,28 @@
+import SigmaR.StandingAlgebra_FormalCore
+import SigmaR.Valuation.ResponseSemantics.Response
+
+/-!
+###############################################################################
+# Response Set
+###############################################################################
+
+This file introduces collections of responses considered together.
+
+A response set does NOT imply:
+• prioritization
+• authorization
+• sequencing
+• selection
+
+It is a descriptive space of possibilities only.
+###############################################################################
+-/
+
+namespace SigmaR
+
+/--
+A set of responses considered jointly.
+-/
+constant ResponseSet : Type
+
+end SigmaR
