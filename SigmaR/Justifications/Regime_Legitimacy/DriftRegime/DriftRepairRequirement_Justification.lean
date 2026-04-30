250a8a61289b72c380a5368a7f29a2c2c326cc5b
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-04-30 16:05:20 -0500

    Update DriftRepairRequirement_Justification.lean

--- SigmaR/Justifications/Regime_Legitimacy/DriftRegime/DriftRepairRequirement_Justification.lean
+++ SigmaR/Justifications/Regime_Legitimacy/DriftRegime/DriftRepairRequirement_Justification.lean
@@ -1,3 +1,5 @@
+module SigmaR.Justifications.Regime_Legitimacy.DriftRegime.DriftRepairRequirement_Justification
+
 import SigmaR.Justifications.Regimes.RemedyRegime.RemedyRegime_Justification
 import SigmaR.Justifications.Regimes.DriftRegime.DriftRegime_Justification
 
