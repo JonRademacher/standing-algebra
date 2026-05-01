2bbcd89b73451c5d27b188f9bd681d90ddc1651d
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-01 15:45:17 -0500

    Create CouplingDoesNotImplyMorality.lean

---
+++ SigmaR/Valuation/DerivedValuations/CouplingValuation/CouplingDoesNotImplyMorality.lean
@@ -0,0 +1,24 @@
+import SigmaR.StandingAlgebra_FormalCore
+
+/-!
+###############################################################################
+# Coupling Does Not Imply Morality
+###############################################################################
+
+Coupling valuation is explicitly non-moral.
+
+It does not encode ethical correctness,
+value judgments, or moral obligation.
+
+This file introduces no axioms.
+###############################################################################
+-/
+
+namespace SigmaR
+
+/--
+Coupling valuation does not imply morality.
+-/
+constant CouplingDoesNotImplyMorality : Prop
+
+end SigmaR
