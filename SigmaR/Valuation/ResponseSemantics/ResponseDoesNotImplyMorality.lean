4910c5e6515eb8535ecb74be524e22cdf0d93eb0
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-01 14:10:40 -0500

    Create ResponseDoesNotImplyMorality.lean

---
+++ SigmaR/Valuation/ResponseSemantics/ResponseDoesNotImplyMorality.lean
@@ -0,0 +1,26 @@
+import SigmaR.StandingAlgebra_FormalCore
+import SigmaR.Valuation.ResponseSemantics.Response
+
+/-!
+###############################################################################
+# Response Does Not Imply Morality
+###############################################################################
+
+Response semantics are explicitly non-moral.
+
+They do not encode value judgments, ethical correctness,
+or normative evaluation.
+
+This file introduces no axioms.
+###############################################################################
+-/
+
+namespace SigmaR
+
+/--
+Response semantics do not imply moral evaluation
+or ethical judgment.
+-/
+constant ResponseDoesNotImplyMorality : Prop
+
+end SigmaR
