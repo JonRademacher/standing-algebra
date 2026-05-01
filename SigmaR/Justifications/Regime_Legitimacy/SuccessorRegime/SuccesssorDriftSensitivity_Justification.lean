75770339682b532bce8abd13448f42f2a43ec1ff
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-01 11:42:46 -0500

    Update SuccesssorDriftSensitivity_Justification.lean

--- SigmaR/Justifications/Regime_Legitimacy/SuccessorRegime/SuccesssorDriftSensitivity_Justification.lean
+++ SigmaR/Justifications/Regime_Legitimacy/SuccessorRegime/SuccesssorDriftSensitivity_Justification.lean
@@ -1,5 +1,3 @@
-module SigmaR.Justifications.Regime_Legitimacy.SuccessorRegime.SuccessorDriftSensitivity_Justification
-
 import SigmaR.StandingAlgebra_FormalCore
 import SigmaR.Justifications.Regimes.SuccessorRegime.SuccessorRegime_Justification
 import SigmaR.Justifications.Regimes.DriftRegime.DriftRegime_Justification
