84920f9dfcfb5cb87ef4513f40e11bd7cbb0ae5c
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-01 10:53:06 -0500

    Update EnablementObligation_Justification.lean

--- SigmaR/Justifications/Core_Legitimacy/EnablementObligation_Justification.lean
+++ SigmaR/Justifications/Core_Legitimacy/EnablementObligation_Justification.lean
@@ -1,5 +1,3 @@
-module SigmaR.Justifications.Core_Legitimacy.EnablementObligation_Justification
-
 import SigmaR.Justifications.Core_Legitimacy.CIA_Justification
 import SigmaR.Justifications.Core_Legitimacy.DerivedStanding_Justification
 
