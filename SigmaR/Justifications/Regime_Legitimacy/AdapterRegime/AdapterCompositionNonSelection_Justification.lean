9cc1282d5bed5401555df61eadb82e709c681880
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-01 11:28:49 -0500

    Update AdapterCompositionNonSelection_Justification.lean

--- SigmaR/Justifications/Regime_Legitimacy/AdapterRegime/AdapterCompositionNonSelection_Justification.lean
+++ SigmaR/Justifications/Regime_Legitimacy/AdapterRegime/AdapterCompositionNonSelection_Justification.lean
@@ -1,5 +1,3 @@
-module SigmaR.Justifications.Regime_Legitimacy.AdapterRegime.AdapterCompositionNonSelection_Justification
-
 import SigmaR.Justifications.Core_Legitimacy.NonSelection_Justification
 import SigmaR.Justifications.Core_Legitimacy.StandingMonotonicity
 import SigmaR.Justifications.Regimes.AdapterRegime.AdapterRegime_Justification
