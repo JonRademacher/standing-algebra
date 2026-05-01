ceb28e5514e49ed7121d60ae857f47e35f7309ae
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-01 15:21:43 -0500

    Create StandingValuationDoesNotImplyMorality.lean

---
+++ SigmaR/Valuation/DerivedValuations/StandingValuation/StandingValuationDoesNotImplyMorality.lean
@@ -0,0 +1,24 @@
+import SigmaR.StandingAlgebra_FormalCore
+
+/-!
+###############################################################################
+# Standing Valuation Does Not Imply Morality
+###############################################################################
+
+Standing valuation is explicitly non-moral.
+
+It does not encode ethical correctness, value judgments,
+or moral obligation.
+
+This file introduces no axioms.
+###############################################################################
+-/
+
+namespace SigmaR
+
+/--
+Standing valuation does not imply morality.
+-/
+constant StandingValuationDoesNotImplyMorality : Prop
+
+end SigmaR
