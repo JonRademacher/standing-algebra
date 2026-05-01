650fcca3b4d169f87d72089c975e0f5c7ed6ba5f
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-01 11:42:25 -0500

    Update SuccessorRepairCompatibility_Justification.lean

--- SigmaR/Justifications/Regime_Legitimacy/SuccessorRegime/SuccessorRepairCompatibility_Justification.lean
+++ SigmaR/Justifications/Regime_Legitimacy/SuccessorRegime/SuccessorRepairCompatibility_Justification.lean
@@ -1,5 +1,3 @@
-module SigmaR.Justifications.Regime_Legitimacy.SuccessorRegime.SuccessorRepairCompatibility_Justification
-
 import SigmaR.Justifications.Core_Legitimacy.DirectedRepair_Justification
 import SigmaR.Justifications.Regimes.SuccessorRegime.SuccessorRegime_Justification
 
