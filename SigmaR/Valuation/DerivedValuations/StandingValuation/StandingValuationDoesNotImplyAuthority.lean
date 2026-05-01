fc239ee8a022af466268cc06658dab38a44bc154
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-01 15:19:06 -0500

    Create StandingValuationDoesNotImplyAuthority.lean

---
+++ SigmaR/Valuation/DerivedValuations/StandingValuation/StandingValuationDoesNotImplyAuthority.lean
@@ -0,0 +1,22 @@
+import SigmaR.StandingAlgebra_FormalCore
+
+/-!
+###############################################################################
+# Standing Valuation Does Not Imply Authority
+###############################################################################
+
+Standing valuation does not grant authority,
+permission, or mandate for action.
+
+This file introduces no axioms.
+###############################################################################
+-/
+
+namespace SigmaR
+
+/--
+Standing valuation does not imply authority.
+-/
+constant StandingValuationDoesNotImplyAuthority : Prop
+
+end SigmaR
