ba30d1847a15cdb54cb2d9c0d9e515d17d786de0
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-01 11:30:03 -0500

    Update EpistemicAdapter_Justification.lean

--- SigmaR/Justifications/Regime_Legitimacy/AdapterRegime/EpistemicAdapter_Justification.lean
+++ SigmaR/Justifications/Regime_Legitimacy/AdapterRegime/EpistemicAdapter_Justification.lean
@@ -1,5 +1,3 @@
-module SigmaR.Justifications.Regime_Legitimacy.AdapterRegime.EpistemicAdapter_Justification
-
 import SigmaR.Justifications.Core_Legitimacy.PFA_Justification
 import SigmaR.Justifications.Core_Legitimacy.ScopeScaleRelativity_Justification
 import SigmaR.Justifications.Core_Legitimacy.DerivedStanding_Justification
