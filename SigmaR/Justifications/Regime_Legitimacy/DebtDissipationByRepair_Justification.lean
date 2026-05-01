67c73f60911efc50026e4084f330754aeff0b88e
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-01 11:28:31 -0500

    Update DebtDissipationByRepair_Justification.lean

--- SigmaR/Justifications/Regime_Legitimacy/DebtDissipationByRepair_Justification.lean
+++ SigmaR/Justifications/Regime_Legitimacy/DebtDissipationByRepair_Justification.lean
@@ -1,5 +1,3 @@
-module SigmaR.Justifications.Regime_Legitimacy.DebtDissipationByRepair_Justification
-
 import SigmaR.Justifications.Core_Legitimacy.StructuralDebt_Justification
 import SigmaR.Justifications.Core_Legitimacy.DirectedRepair_Justification
 
