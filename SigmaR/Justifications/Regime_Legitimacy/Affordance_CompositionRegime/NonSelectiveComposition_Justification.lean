592f6ccdddce5c5a3cb75c9fdadadcab309f49b3
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-01 11:30:39 -0500

    Update NonSelectiveComposition_Justification.lean

--- SigmaR/Justifications/Regime_Legitimacy/Affordance_CompositionRegime/NonSelectiveComposition_Justification.lean
+++ SigmaR/Justifications/Regime_Legitimacy/Affordance_CompositionRegime/NonSelectiveComposition_Justification.lean
@@ -1,5 +1,3 @@
-module SigmaR.Justifications.Regime_Legitimacy.Affordance_CompositionRegime.NonSelectiveComposition_Justification
-
 import SigmaR.Justifications.Core_Legitimacy.NonSelection_Justification
 import SigmaR.Justifications.Regimes.Affordance_CompositionRegime.AffordanceCompositionRegime_Justification
 
