f8f16141e183829e782b2f8599d0137bef5066ba
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-01 11:34:29 -0500

    Update RiskBoundEmergency_Justification.lean

--- SigmaR/Justifications/Regime_Legitimacy/EmergencyRegime/RiskBoundEmergency_Justification.lean
+++ SigmaR/Justifications/Regime_Legitimacy/EmergencyRegime/RiskBoundEmergency_Justification.lean
@@ -1,5 +1,3 @@
-module SigmaR.Justifications.Regime_Legitimacy.EmergencyRegime.RiskBoundEmergency_Justification
-
 import SigmaR.Justifications.Core_Legitimacy.CIA_Justification
 import SigmaR.Justifications.Core_Legitimacy.StructuralDebt_Justification
 import SigmaR.Justifications.Core_Legitimacy.DirectedRepair_Justification
