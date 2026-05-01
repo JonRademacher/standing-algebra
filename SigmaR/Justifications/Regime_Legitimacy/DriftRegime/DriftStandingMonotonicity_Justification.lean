d86314ff513adb5faf2bb86235ccfafb8f0b29ef
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-01 11:33:17 -0500

    Update DriftStandingMonotonicity_Justification.lean

--- SigmaR/Justifications/Regime_Legitimacy/DriftRegime/DriftStandingMonotonicity_Justification.lean
+++ SigmaR/Justifications/Regime_Legitimacy/DriftRegime/DriftStandingMonotonicity_Justification.lean
@@ -1,5 +1,3 @@
-module SigmaR.Justifications.Regime_Legitimacy.DriftRegime.DriftStandingMonotonicity_Justification
-
 import SigmaR.Justifications.Core_Legitimacy.ALRP_Justification
 import SigmaR.Justifications.Regimes.DriftRegime.DriftRegime_Justification
 
