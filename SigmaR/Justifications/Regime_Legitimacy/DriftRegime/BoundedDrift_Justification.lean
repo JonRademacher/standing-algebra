a8f9108c81a5d7f06cf565be06991e1d025ce4da
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-01 11:31:50 -0500

    Update BoundedDrift_Justification.lean

--- SigmaR/Justifications/Regime_Legitimacy/DriftRegime/BoundedDrift_Justification.lean
+++ SigmaR/Justifications/Regime_Legitimacy/DriftRegime/BoundedDrift_Justification.lean
@@ -1,5 +1,3 @@
-module SigmaR.Justifications.Regime_Legitimacy.DriftRegime.BoundedDrift_Justification
-
 import SigmaR.StandingAlgebra_FormalCore
 import SigmaR.Justifications.Regimes.DriftRegime.DriftRegime_Justification
 
