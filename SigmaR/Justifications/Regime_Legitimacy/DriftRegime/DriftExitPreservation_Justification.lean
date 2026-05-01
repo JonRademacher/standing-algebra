b59b1446962437fff437d2b1771fdb112e763c06
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-01 11:32:23 -0500

    Update DriftExitPreservation_Justification.lean

--- SigmaR/Justifications/Regime_Legitimacy/DriftRegime/DriftExitPreservation_Justification.lean
+++ SigmaR/Justifications/Regime_Legitimacy/DriftRegime/DriftExitPreservation_Justification.lean
@@ -1,5 +1,3 @@
-module SigmaR.Justifications.Regime_Legitimacy.DriftRegime.DriftExitPreservation_Justification
-
 import SigmaR.Justifications.Regimes.ExitRegime.ExitRegime_Justification
 import SigmaR.Justifications.Regimes.DriftRegime.DriftRegime_Justification
 
