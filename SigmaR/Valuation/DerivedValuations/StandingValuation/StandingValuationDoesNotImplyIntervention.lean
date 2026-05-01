303697c1ad71dd08b3b2e3d838d4fbbd28640294
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-01 15:21:18 -0500

    Create StandingValuationDoesNotImplyIntervention.lean

---
+++ SigmaR/Valuation/DerivedValuations/StandingValuation/StandingValuationDoesNotImplyIntervention.lean
@@ -0,0 +1,22 @@
+import SigmaR.StandingAlgebra_FormalCore
+
+/-!
+###############################################################################
+# Standing Valuation Does Not Imply Intervention
+###############################################################################
+
+Standing valuation does not authorize, mandate,
+or justify intervention or action.
+
+This file introduces no axioms.
+###############################################################################
+-/
+
+namespace SigmaR
+
+/--
+Standing valuation does not imply intervention.
+-/
+constant StandingValuationDoesNotImplyIntervention : Prop
+
+end SigmaR
