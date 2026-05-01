fb438ce2fefecfc4b724fbeb105562d8d90d5250
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-01 14:43:49 -0500

    Create ValuationDoesNotImplyLegitimacy.lean

---
+++ SigmaR/Valuation/DerivedValuations/ValuationDoesNotImplyLegitimacy.lean
@@ -0,0 +1,22 @@
+import SigmaR.StandingAlgebra_FormalCore
+
+/-!
+###############################################################################
+# Valuation Does Not Imply Legitimacy
+###############################################################################
+
+Derived valuations do not imply moral, legal,
+or institutional legitimacy.
+
+This file introduces no axioms.
+###############################################################################
+-/
+
+namespace SigmaR
+
+/--
+Derived valuation does not imply legitimacy.
+-/
+constant ValuationDoesNotImplyLegitimacy : Prop
+
+end SigmaR
