853a2878df2e595104bdd3d051e2a71c639bc7e5
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-01 14:09:33 -0500

    Create ResponseDoesNotImplyAuthority.lean

---
+++ SigmaR/Valuation/ResponseSemantics/ResponseDoesNotImplyAuthority.lean
@@ -0,0 +1,24 @@
+import SigmaR.StandingAlgebra_FormalCore
+import SigmaR.Valuation.ResponseSemantics.Response
+
+/-!
+###############################################################################
+# Response Does Not Imply Authority
+###############################################################################
+
+The existence or characterization of a response does not
+grant authority, permission, or mandate to act.
+
+This file introduces no axioms.
+###############################################################################
+-/
+
+namespace SigmaR
+
+/--
+Response semantics do not imply authority to enact,
+enforce, or prioritize a response.
+-/
+constant ResponseDoesNotImplyAuthority : Prop
+
+end SigmaR
