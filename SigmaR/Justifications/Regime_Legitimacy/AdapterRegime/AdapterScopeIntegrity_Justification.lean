60c0d35638ea75f3730850e1f2311a4fa8fd5011
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-01 11:29:53 -0500

    Update AdapterScopeIntegrity_Justification.lean

--- SigmaR/Justifications/Regime_Legitimacy/AdapterRegime/AdapterScopeIntegrity_Justification.lean
+++ SigmaR/Justifications/Regime_Legitimacy/AdapterRegime/AdapterScopeIntegrity_Justification.lean
@@ -1,5 +1,3 @@
-module SigmaR.Justifications.Regime_Legitimacy.AdapterRegime.AdapterScopeIntegrity_Justification
-
 import SigmaR.Justifications.Core_Legitimacy.ScopeScaleRelativity_Justification
 import SigmaR.Justifications.Core_Legitimacy.MultiScopeSimultaneity_Justification
 import SigmaR.Justifications.Core_Legitimacy.PublicFrameResolution_Justification
