38003fbbf5c508497d0d52a5305f2e1a382e7fbf
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-01 11:35:40 -0500

    Update ExitRegime_Justification.lean

--- SigmaR/Justifications/Regime_Legitimacy/ExitRegime/ExitRegime_Justification.lean
+++ SigmaR/Justifications/Regime_Legitimacy/ExitRegime/ExitRegime_Justification.lean
@@ -1,5 +1,3 @@
-module SigmaR.Justifications.Regime_Legitimacy.ExitRegime.ExitRegime_Justification
-
 import SigmaR.Justifications.Core_Legitimacy.StructuralDebt_Justification
 import SigmaR.Justifications.Core_Legitimacy.DirectedRepair_Justification
 import SigmaR.Justifications.Core_Legitimacy.LegitimacyEnvelope_Justification
