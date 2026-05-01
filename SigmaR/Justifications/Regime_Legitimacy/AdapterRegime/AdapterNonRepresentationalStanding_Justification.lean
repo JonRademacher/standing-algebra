fa136d5327ffb4f1f772b9f2c9973eabd2812c80
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-01 11:29:23 -0500

    Update AdapterNonRepresentationalStanding_Justification.lean

--- SigmaR/Justifications/Regime_Legitimacy/AdapterRegime/AdapterNonRepresentationalStanding_Justification.lean
+++ SigmaR/Justifications/Regime_Legitimacy/AdapterRegime/AdapterNonRepresentationalStanding_Justification.lean
@@ -1,5 +1,3 @@
-module SigmaR.Justifications.Regime_Legitimacy.AdapterRegime.AdapterNonRepresentationalStanding_Justification
-
 import SigmaR.Justifications.Core_Legitimacy.DerivedStanding_Justification
 import SigmaR.Justifications.Regimes.AdapterRegime.AdapterRegime_Justification
 import SigmaR.Justifications.Regimes.RiskRegime.RiskInheritance_Justification
