aa36dbeca4fda70937f8be305c19c8c59a97b190
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-01 11:35:17 -0500

    Update ExitNonSubstitutability_Justification.lean

--- SigmaR/Justifications/Regime_Legitimacy/ExitRegime/ExitNonSubstitutability_Justification.lean
+++ SigmaR/Justifications/Regime_Legitimacy/ExitRegime/ExitNonSubstitutability_Justification.lean
@@ -1,5 +1,3 @@
-module SigmaR.Justifications.Regime_Legitimacy.ExitRegime.ExitNonSubstitutability_Justification
-
 import SigmaR.Justifications.Regimes.ExitRegime.ExitRegime_Justification
 import SigmaR.Justifications.Core_Legitimacy.ContestationCondition_Justification
 
