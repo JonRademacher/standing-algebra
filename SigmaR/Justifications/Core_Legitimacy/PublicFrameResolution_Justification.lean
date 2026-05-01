a1616ca054d41327810afb7433eef72c3eb93876
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-01 11:16:21 -0500

    Update PublicFrameResolution_Justification.lean

--- SigmaR/Justifications/Core_Legitimacy/PublicFrameResolution_Justification.lean
+++ SigmaR/Justifications/Core_Legitimacy/PublicFrameResolution_Justification.lean
@@ -1,5 +1,3 @@
-module SigmaR.Justifications.Core_Legitimacy.PublicFrameResolution_Justification
-
 import SigmaR.Justifications.Core_Legitimacy.PFA_Justification
 import SigmaR.Justifications.Core_Legitimacy.ContestationCondition_Justification
 import SigmaR.Justifications.Core_Legitimacy.MultiScopeSimultaneity_Justification
