d4f44aa7036398fa439a54d12e95b2d2c286d4bf
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-01 11:41:29 -0500

    Update SuccessorContinuation_Justification.lean

--- SigmaR/Justifications/Regime_Legitimacy/SuccessorRegime/SuccessorContinuation_Justification.lean
+++ SigmaR/Justifications/Regime_Legitimacy/SuccessorRegime/SuccessorContinuation_Justification.lean
@@ -1,5 +1,3 @@
-module SigmaR.Justifications.Regime_Legitimacy.SuccessorRegime.SuccessorContinuation_Justification
-
 import SigmaR.Justifications.Regimes.SuccessorRegime.SuccessorRegime_Justification
 import SigmaR.Justifications.Regimes.ExitRegime.ExitRegime_Justification
 import SigmaR.Justifications.Regimes.RemedyRegime.RemedyRegime_Justification
