e609712cbb32ceb58055782767f137e725c8d95d
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-01 14:44:39 -0500

    Create ValuationDoesNotImplyIntervention.lean

---
+++ SigmaR/Valuation/DerivedValuations/ValuationDoesNotImplyIntervention.lean
@@ -0,0 +1,22 @@
+import SigmaR.StandingAlgebra_FormalCore
+
+/-!
+###############################################################################
+# Valuation Does Not Imply Intervention
+###############################################################################
+
+Derived valuations do not authorize, mandate,
+or justify intervention or action.
+
+This file introduces no axioms.
+###############################################################################
+-/
+
+namespace SigmaR
+
+/--
+Derived valuation does not imply intervention.
+-/
+constant ValuationDoesNotImplyIntervention : Prop
+
+end SigmaR
