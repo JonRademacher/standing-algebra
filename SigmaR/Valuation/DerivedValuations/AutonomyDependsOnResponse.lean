8b7b2dc3299615064b4d3fc392145f21c9cf7412
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-01 14:31:08 -0500

    Create AutonomyDependsOnResponse.lean

---
+++ SigmaR/Valuation/DerivedValuations/AutonomyDependsOnResponse.lean
@@ -0,0 +1,31 @@
+import SigmaR.StandingAlgebra_FormalCore
+import SigmaR.Valuation.DerivedValuations.AutonomyValuation
+import SigmaR.Valuation.ResponseSemantics.Response
+
+/-!
+###############################################################################
+# Autonomy Depends On Response
+###############################################################################
+
+This file introduces the structural dependency of autonomy
+valuation on response semantics.
+
+Dependency does NOT imply:
+• authorization of response
+• desirability of response
+• intervention
+
+It is purely structural sensitivity.
+###############################################################################
+-/
+
+namespace SigmaR
+
+/--
+AutonomyDependsOnResponse A R means that autonomy valuation A
+is structurally sensitive to response R.
+-/
+constant AutonomyDependsOnResponse :
+  AutonomyValuation → Response → Prop
+
+end SigmaR
