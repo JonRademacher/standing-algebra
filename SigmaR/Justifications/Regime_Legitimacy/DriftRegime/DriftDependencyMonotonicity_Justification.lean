3166f343d525e2e854be4d37b2c6f3d101a6a61c
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-01 11:32:13 -0500

    Update DriftDependencyMonotonicity_Justification.lean

--- SigmaR/Justifications/Regime_Legitimacy/DriftRegime/DriftDependencyMonotonicity_Justification.lean
+++ SigmaR/Justifications/Regime_Legitimacy/DriftRegime/DriftDependencyMonotonicity_Justification.lean
@@ -1,5 +1,3 @@
-module SigmaR.Justifications.Regime_Legitimacy.DriftRegime.DriftDependencyMonotonicity_Justification
-
 import SigmaR.Justifications.Core_Legitimacy.NRPP_Justification
 import SigmaR.Justifications.Regimes.DriftRegime.DriftRegime_Justification
 
