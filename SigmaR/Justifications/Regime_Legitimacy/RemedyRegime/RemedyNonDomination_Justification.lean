43d6d4dae66d1279a81da6306fbd68bf92e498a3
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-01 11:36:30 -0500

    Update RemedyNonDomination_Justification.lean

--- SigmaR/Justifications/Regime_Legitimacy/RemedyRegime/RemedyNonDomination_Justification.lean
+++ SigmaR/Justifications/Regime_Legitimacy/RemedyRegime/RemedyNonDomination_Justification.lean
@@ -1,5 +1,3 @@
-module SigmaR.Justifications.Regime_Legitimacy.RemedyRegime.RemedyNonDomination_Justification
-
 import SigmaR.Justifications.Core_Legitimacy.DominantCoupling_Justification
 import SigmaR.Justifications.Regimes.RemedyRegime.RemedyRegime_Justification
 
