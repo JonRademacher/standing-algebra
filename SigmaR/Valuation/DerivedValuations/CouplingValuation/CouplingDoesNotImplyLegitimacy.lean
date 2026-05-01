5d57149e3b36c8b5226e626c1f7ac4504c8750ed
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-01 15:44:06 -0500

    Create CouplingDoesNotImplyLegitimacy.lean

---
+++ SigmaR/Valuation/DerivedValuations/CouplingValuation/CouplingDoesNotImplyLegitimacy.lean
@@ -0,0 +1,22 @@
+import SigmaR.StandingAlgebra_FormalCore
+
+/-!
+###############################################################################
+# Coupling Does Not Imply Legitimacy
+###############################################################################
+
+Coupling valuation does not imply moral, legal,
+or institutional legitimacy.
+
+This file introduces no axioms.
+###############################################################################
+-/
+
+namespace SigmaR
+
+/--
+Coupling valuation does not imply legitimacy.
+-/
+constant CouplingDoesNotImplyLegitimacy : Prop
+
+end SigmaR
