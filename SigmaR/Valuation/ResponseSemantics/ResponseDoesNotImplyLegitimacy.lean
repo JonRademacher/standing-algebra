33bedf878a852e98ab739d1baf634eee01e38800
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-01 14:10:21 -0500

    Create ResponseDoesNotImplyLegitimacy.lean

---
+++ SigmaR/Valuation/ResponseSemantics/ResponseDoesNotImplyLegitimacy.lean
@@ -0,0 +1,23 @@
+import SigmaR.StandingAlgebra_FormalCore
+import SigmaR.Valuation.ResponseSemantics.Response
+
+/-!
+###############################################################################
+# Response Does Not Imply Legitimacy
+###############################################################################
+
+Response structure does not imply moral, legal, or
+institutional legitimacy.
+
+This file introduces no axioms.
+###############################################################################
+-/
+
+namespace SigmaR
+
+/--
+Response semantics do not imply legitimacy.
+-/
+constant ResponseDoesNotImplyLegitimacy : Prop
+
+end SigmaR
