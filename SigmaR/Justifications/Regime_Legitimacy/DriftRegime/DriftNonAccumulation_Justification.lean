b8da33260ca68e077ef7e65497d8f45e8420bcdb
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-01 11:32:34 -0500

    Update DriftNonAccumulation_Justification.lean

--- SigmaR/Justifications/Regime_Legitimacy/DriftRegime/DriftNonAccumulation_Justification.lean
+++ SigmaR/Justifications/Regime_Legitimacy/DriftRegime/DriftNonAccumulation_Justification.lean
@@ -1,5 +1,3 @@
-module SigmaR.Justifications.Regime_Legitimacy.DriftRegime.DriftNonAccumulation_Justification
-
 import SigmaR.StandingAlgebra_FormalCore
 import SigmaR.Justifications.Regimes.DriftRegime.DriftRegime_Justification
 
