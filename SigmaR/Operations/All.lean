86e2afe173c3cb45fcbf6613ff257ec7bf93edcb
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-01 12:39:52 -0500

    Create All.lean

---
+++ SigmaR/Operations/All.lean
@@ -0,0 +1,35 @@
+import SigmaR.Operations.LegitimacyInterface
+import SigmaR.Operations.AdmissibilityInvariant
+
+-- -------------------------------
+-- Operations (constructive / admissible)
+-- -------------------------------
+
+import SigmaR.Operations.Compromise_Op
+import SigmaR.Operations.ContinuationStability_Op
+import SigmaR.Operations.Cooperation_Op
+import SigmaR.Operations.CoreConstraintPersistence_Op
+import SigmaR.Operations.Negotiation_Op
+import SigmaR.Operations.ProceduralVsJustificatoryExclusivity_Op
+import SigmaR.Operations.RegimeInteractionNormalization_Op
+import SigmaR.Operations.ResolutionFallback_Op
+import SigmaR.Operations.ResolutionMethodDistinctness_Op
+import SigmaR.Operations.ResolutionModeSoundness_Op
+import SigmaR.Operations.SuccessorContinuation_Op
+import SigmaR.Operations.TragicAcceptance_Op
+
+-- -------------------------------
+-- Illegitimacy / inadmissibility theorems
+-- -------------------------------
+
+import SigmaR.Operations.DebtWithoutRepair_Illegitimacy
+import SigmaR.Operations.DominantCoupling_Illegitimacy
+import SigmaR.Operations.EmergencyWithoutContraction_Illegitimacy
+import SigmaR.Operations.ExitDenial_InducesDominantCoupling
+import SigmaR.Operations.RiskExitInteraction_Illegitimacy
+
+-- -------------------------------
+-- Aggregation / flagship results
+-- -------------------------------
+
+import SigmaR.Operations.FlagshipIllegitimacyTheorems
