a785b1d2f52e5888c4d3a735d25c9bea9e1e7dff
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-01 14:09:56 -0500

    Create ResponseDoesNotImplyIntervention.lean

---
+++ SigmaR/Valuation/ResponseSemantics/ResponseDoesNotImplyIntervention.lean
@@ -0,0 +1,24 @@
+import SigmaR.StandingAlgebra_FormalCore
+import SigmaR.Valuation.ResponseSemantics.Response
+
+/-!
+###############################################################################
+# Response Does Not Imply Intervention
+###############################################################################
+
+The availability or admissibility of a response does not
+imply that an intervention should or must occur.
+
+This file introduces no axioms.
+###############################################################################
+-/
+
+namespace SigmaR
+
+/--
+Response semantics do not imply intervention,
+execution, or action.
+-/
+constant ResponseDoesNotImplyIntervention : Prop
+
+end SigmaR
