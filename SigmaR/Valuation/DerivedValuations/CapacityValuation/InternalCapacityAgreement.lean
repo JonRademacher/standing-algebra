427223f2c747036e07fe6bb03d6b0880b60f5d3c
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-04 11:58:08 -0500

    Create InternalCapacityAgreement.lean

---
+++ SigmaR/Valuation/DerivedValuations/CapacityValuation/InternalCapacityAgreement.lean
@@ -0,0 +1,29 @@
+import SigmaR.StandingAlgebra_FormalCore
+import SigmaR.Valuation.DerivedValuations.CapacityAgreement
+
+/-!
+###############################################################################
+# Internal Capacity Agreement
+###############################################################################
+
+This file introduces internalized capacity agreement.
+
+Internal capacity agreement reflects an agent’s stabilized
+self-assessment of effective capacity, potentially accumulated
+over time.
+
+It grounds personal warrant and does NOT depend on
+external validation.
+###############################################################################
+-/
+
+namespace SigmaR
+
+/--
+InternalCapacityAgreement A means that capacity agreement A
+is internalized within a single agent.
+-/
+constant InternalCapacityAgreement :
+  CapacityAgreement → Prop
+
+end SigmaR
