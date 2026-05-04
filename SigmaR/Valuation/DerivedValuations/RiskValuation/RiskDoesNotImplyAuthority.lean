53632f8e959a24f6333994d26304372de18f582e
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-04 14:52:31 -0500

    Create RiskDoesNotImplyAuthority.lean

---
+++ SigmaR/Valuation/DerivedValuations/RiskValuation/RiskDoesNotImplyAuthority.lean
@@ -0,0 +1,22 @@
+import SigmaR.StandingAlgebra_FormalCore
+
+/-!
+###############################################################################
+# Risk Does Not Imply Authority
+###############################################################################
+
+Risk valuation does not grant authority, control,
+or decision-making power.
+
+This file introduces no axioms.
+###############################################################################
+-/
+
+namespace SigmaR
+
+/--
+Risk valuation does not imply authority.
+-/
+constant RiskDoesNotImplyAuthority : Prop
+
+end SigmaR
