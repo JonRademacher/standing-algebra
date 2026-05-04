e2eb5ccbcc0b9c528c3aaaaa35c2e1f66d2968bb
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-04 09:23:58 -0500

    Create ExitCostDoesNotImplyIntervention.lean

---
+++ SigmaR/Valuation/DerivedValuations/ExitCostValuation/ExitCostDoesNotImplyIntervention.lean
@@ -0,0 +1,22 @@
+import SigmaR.StandingAlgebra_FormalCore
+
+/-!
+###############################################################################
+# Exit Cost Does Not Imply Intervention
+###############################################################################
+
+Exit cost valuation does not authorize, mandate,
+or justify intervention, coercion, or action.
+
+This file introduces no axioms.
+###############################################################################
+-/
+
+namespace SigmaR
+
+/--
+Exit cost valuation does not imply intervention.
+-/
+constant ExitCostDoesNotImplyIntervention : Prop
+
+end SigmaR
