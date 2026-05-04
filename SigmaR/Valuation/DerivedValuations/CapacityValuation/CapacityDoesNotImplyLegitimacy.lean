092d7eaa899c1662fde23eacbaf4663391da8800
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-04 13:05:50 -0500

    Create CapacityDoesNotImplyLegitimacy.lean

---
+++ SigmaR/Valuation/DerivedValuations/CapacityValuation/CapacityDoesNotImplyLegitimacy.lean
@@ -0,0 +1,25 @@
+import SigmaR.StandingAlgebra_FormalCore
+
+/-!
+###############################################################################
+# Capacity Does Not Imply Legitimacy
+###############################################################################
+
+Capacity valuation does not imply moral, legal,
+or institutional legitimacy.
+
+This blocks conversion of capacity assessment
+into normative standing.
+
+This file introduces no axioms.
+###############################################################################
+-/
+
+namespace SigmaR
+
+/--
+Capacity valuation does not imply legitimacy.
+-/
+constant CapacityDoesNotImplyLegitimacy : Prop
+
+end SigmaR
