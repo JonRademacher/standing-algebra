a4444eacf0a812b30424fd2a391ed5cd95508837
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-04 14:02:23 -0500

    Create TransformabilityProgressOption.lean

---
+++ SigmaR/Valuation/DerivedValuations/TransformabilityValuation/TransformabilityProgressOption.lean
@@ -0,0 +1,26 @@
+import SigmaR.StandingAlgebra_FormalCore
+
+/-!
+###############################################################################
+# Transformability Progress Option
+###############################################################################
+
+This file introduces the notion of a transformability-progress
+option.
+
+A transformability-progress option represents the existence of
+a path that would strictly increase effective transformability.
+
+It is a structural possibility, not a preference.
+###############################################################################
+-/
+
+namespace SigmaR
+
+/--
+TransformabilityProgressOption O means that O represents
+a transformability-increasing option.
+-/
+constant TransformabilityProgressOption : Type
+
+end SigmaR
