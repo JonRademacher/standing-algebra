decfc45d4f0be01e9f96267c4b6741d8d03d198f
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-04 13:06:44 -0500

    Create CapacityDoesNotImplyMorality.lean

---
+++ SigmaR/Valuation/DerivedValuations/CapacityValuation/CapacityDoesNotImplyMorality.lean
@@ -0,0 +1,24 @@
+import SigmaR.StandingAlgebra_FormalCore
+
+/-!
+###############################################################################
+# Capacity Does Not Imply Morality
+###############################################################################
+
+Capacity valuation is explicitly non-moral.
+
+It does not encode ethical judgment, blame,
+or obligation.
+
+This file introduces no axioms.
+###############################################################################
+-/
+
+namespace SigmaR
+
+/--
+Capacity valuation does not imply morality.
+-/
+constant CapacityDoesNotImplyMorality : Prop
+
+end SigmaR
