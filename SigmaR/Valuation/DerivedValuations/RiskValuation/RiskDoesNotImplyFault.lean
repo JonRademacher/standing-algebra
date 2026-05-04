54a4d76703f073003ba0b1c5c64c86f8621c1201
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-04 14:51:39 -0500

    Create RiskDoesNotImplyFault.lean

---
+++ SigmaR/Valuation/DerivedValuations/RiskValuation/RiskDoesNotImplyFault.lean
@@ -0,0 +1,24 @@
+import SigmaR.StandingAlgebra_FormalCore
+
+/-!
+###############################################################################
+# Risk Does Not Imply Fault
+###############################################################################
+
+Risk valuation does not imply fault or wrongdoing.
+
+This blocks the inference that exposure to risk
+constitutes responsibility for harm.
+
+This file introduces no axioms.
+###############################################################################
+-/
+
+namespace SigmaR
+
+/--
+Risk valuation does not imply fault.
+-/
+constant RiskDoesNotImplyFault : Prop
+
+end SigmaR
