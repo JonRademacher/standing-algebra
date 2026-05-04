f3ca40ebbf9e2c2cbf533af9cd5b825e67ab1542
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-04 09:24:20 -0500

    Create ExitCostDoesNotImplyMorality.lean

---
+++ SigmaR/Valuation/DerivedValuations/ExitCostValuation/ExitCostDoesNotImplyMorality.lean
@@ -0,0 +1,24 @@
+import SigmaR.StandingAlgebra_FormalCore
+
+/-!
+###############################################################################
+# Exit Cost Does Not Imply Morality
+###############################################################################
+
+Exit cost valuation is explicitly non-moral.
+
+It does not encode ethical correctness,
+value judgments, or moral obligation.
+
+This file introduces no axioms.
+###############################################################################
+-/
+
+namespace SigmaR
+
+/--
+Exit cost valuation does not imply morality.
+-/
+constant ExitCostDoesNotImplyMorality : Prop
+
+end SigmaR
