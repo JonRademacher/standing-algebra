53b639d22642905b0cc12e98910519d9855985da
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-04 14:10:41 -0500

    Create TransformabilityDoesNotImplyMorality.lean

---
+++ SigmaR/Valuation/DerivedValuations/TransformabilityValuation/TransformabilityDoesNotImplyMorality.lean
@@ -0,0 +1,24 @@
+import SigmaR.StandingAlgebra_FormalCore
+
+/-!
+###############################################################################
+# Transformability Does Not Imply Morality
+###############################################################################
+
+Transformability valuation is explicitly non-moral.
+
+It does not encode ethical judgment, blame,
+or obligation.
+
+This file introduces no axioms.
+###############################################################################
+-/
+
+namespace SigmaR
+
+/--
+Transformability valuation does not imply morality.
+-/
+constant TransformabilityDoesNotImplyMorality : Prop
+
+end SigmaR
