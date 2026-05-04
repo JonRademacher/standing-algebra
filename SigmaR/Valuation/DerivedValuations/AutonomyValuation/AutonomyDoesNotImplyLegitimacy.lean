3c8462b52d351e9f52ddd2ae903b13c50144d9c5
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-04 16:35:30 -0500

    Create AutonomyDoesNotImplyLegitimacy.lean

---
+++ SigmaR/Valuation/DerivedValuations/AutonomyValuation/AutonomyDoesNotImplyLegitimacy.lean
@@ -0,0 +1,25 @@
+import SigmaR.StandingAlgebra_FormalCore
+
+/-!
+###############################################################################
+# Autonomy Does Not Imply Legitimacy
+###############################################################################
+
+Autonomy valuation does not imply moral, legal,
+or institutional legitimacy.
+
+This blocks conversion of autonomy assessment
+into normative standing or justification.
+
+This file introduces no axioms.
+###############################################################################
+-/
+
+namespace SigmaR
+
+/--
+Autonomy valuation does not imply legitimacy.
+-/
+constant AutonomyDoesNotImplyLegitimacy : Prop
+
+end SigmaR
