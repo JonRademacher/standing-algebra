f4b60b4507aa853da413db61b32ff84e0d872947
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-04 11:35:06 -0500

    Create PerceptionDoesNotImplyAuthority.lean

---
+++ SigmaR/Valuation/DerivedValuations/PerceptualValuation/PerceptionDoesNotImplyAuthority.lean
@@ -0,0 +1,23 @@
+import SigmaR.StandingAlgebra_FormalCore
+
+/-!
+###############################################################################
+# Perception Does Not Imply Authority
+###############################################################################
+
+Perception valuation does not grant authority
+to dictate belief, enforce agreement, or override
+personal perception.
+
+This file introduces no axioms.
+###############################################################################
+-/
+
+namespace SigmaR
+
+/--
+Perception valuation does not imply authority.
+-/
+constant PerceptionDoesNotImplyAuthority : Prop
+
+end SigmaR
