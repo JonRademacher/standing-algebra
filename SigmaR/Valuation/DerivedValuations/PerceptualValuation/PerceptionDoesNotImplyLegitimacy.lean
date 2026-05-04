09a8787e1645748169c1b1c63082ee2c6dc3e118
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-04 11:34:35 -0500

    Create PerceptionDoesNotImplyLegitimacy.lean

---
+++ SigmaR/Valuation/DerivedValuations/PerceptualValuation/PerceptionDoesNotImplyLegitimacy.lean
@@ -0,0 +1,25 @@
+import SigmaR.StandingAlgebra_FormalCore
+
+/-!
+###############################################################################
+# Perception Does Not Imply Legitimacy
+###############################################################################
+
+Perception valuation does not imply moral, social,
+legal, or institutional legitimacy.
+
+This blocks conversion of perceptual agreement
+into normative standing.
+
+This file introduces no axioms.
+###############################################################################
+-/
+
+namespace SigmaR
+
+/--
+Perception valuation does not imply legitimacy.
+-/
+constant PerceptionDoesNotImplyLegitimacy : Prop
+
+end SigmaR
