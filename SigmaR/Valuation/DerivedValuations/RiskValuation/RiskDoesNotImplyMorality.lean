965a1c9ee3e75f0eecfa16a9565c76e2d5706b91
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-04 14:53:08 -0500

    Create RiskDoesNotImplyMorality.lean

---
+++ SigmaR/Valuation/DerivedValuations/RiskValuation/RiskDoesNotImplyMorality.lean
@@ -0,0 +1,24 @@
+import SigmaR.StandingAlgebra_FormalCore
+
+/-!
+###############################################################################
+# Risk Does Not Imply Morality
+###############################################################################
+
+Risk valuation is explicitly non-moral.
+
+It does not encode ethical judgment, blame,
+or obligation.
+
+This file introduces no axioms.
+###############################################################################
+-/
+
+namespace SigmaR
+
+/--
+Risk valuation does not imply morality.
+-/
+constant RiskDoesNotImplyMorality : Prop
+
+end SigmaR
