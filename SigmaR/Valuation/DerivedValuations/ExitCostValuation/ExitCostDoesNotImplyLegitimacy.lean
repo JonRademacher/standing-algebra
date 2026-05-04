bf38393e6f0f80f98efb1d7073fa073d98040e65
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-04 09:04:18 -0500

    Create ExitCostDoesNotImplyLegitimacy.lean

---
+++ SigmaR/Valuation/DerivedValuations/ExitCostValuation/ExitCostDoesNotImplyLegitimacy.lean
@@ -0,0 +1,22 @@
+import SigmaR.StandingAlgebra_FormalCore
+
+/-!
+###############################################################################
+# Exit Cost Does Not Imply Legitimacy
+###############################################################################
+
+Exit cost valuation does not imply moral, legal,
+or institutional legitimacy.
+
+This file introduces no axioms.
+###############################################################################
+-/
+
+namespace SigmaR
+
+/--
+Exit cost valuation does not imply legitimacy.
+-/
+constant ExitCostDoesNotImplyLegitimacy : Prop
+
+end SigmaR
