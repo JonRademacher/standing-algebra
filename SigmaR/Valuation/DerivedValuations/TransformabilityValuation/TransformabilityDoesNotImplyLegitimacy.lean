8dae122f2640174f23994209707c137bb0587fc2
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-04 14:09:30 -0500

    Create TransformabilityDoesNotImplyLegitimacy.lean

---
+++ SigmaR/Valuation/DerivedValuations/TransformabilityValuation/TransformabilityDoesNotImplyLegitimacy.lean
@@ -0,0 +1,25 @@
+import SigmaR.StandingAlgebra_FormalCore
+
+/-!
+###############################################################################
+# Transformability Does Not Imply Legitimacy
+###############################################################################
+
+Transformability valuation does not imply moral, legal,
+or institutional legitimacy.
+
+This prevents conversion of transformability assessment
+into normative standing.
+
+This file introduces no axioms.
+###############################################################################
+-/
+
+namespace SigmaR
+
+/--
+Transformability valuation does not imply legitimacy.
+-/
+constant TransformabilityDoesNotImplyLegitimacy : Prop
+
+end SigmaR
