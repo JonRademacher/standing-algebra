c77dc9ab20accc4fe776b01f7f869dd53d4a31c2
JonRademacher <146857235+JonRademacher@users.noreply.github.com>
2026-05-04 14:09:53 -0500

    Create TransformabilityDoesNotImplyAuthority.lean

---
+++ SigmaR/Valuation/DerivedValuations/TransformabilityValuation/TransformabilityDoesNotImplyAuthority.lean
@@ -0,0 +1,22 @@
+import SigmaR.StandingAlgebra_FormalCore
+
+/-!
+###############################################################################
+# Transformability Does Not Imply Authority
+###############################################################################
+
+Transformability valuation does not grant authority,
+control, or decision-making power.
+
+This file introduces no axioms.
+###############################################################################
+-/
+
+namespace SigmaR
+
+/--
+Transformability valuation does not imply authority.
+-/
+constant TransformabilityDoesNotImplyAuthority : Prop
+
+end SigmaR
